b1b1976c6f03aafcef5573f4ea5027c8d294c919
pradumangoyal <goyal.praduman611@gmail.com>
2018-12-01 21:51:25 +0530

    Add Conact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=63)
+    email = models.EmailField()
+    subject = models.TextField(blank=True)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    client_ip = models.CharField(max_length=63, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('created',)
